f4683f27d6bedcedcc4905b5fb1817ead589d11f
Lucas Vogel <contact@vogelino.ch>
2016-04-26 23:02:38 +0200

    Added gulp with a configuration that allows compiling from es2015 to es5

---
+++ gulpfile.js/index.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var clean = require('gulp-clean');
+var argv = require('yargs').argv;
+var util = require('gulp-util');
+var source = require('vinyl-source-stream');
+var filesUtil = require('./util/filesUtil');
+
+var srcRoot = './src';
+var distRoot = './dist';
+var paths = {
+	src: {
+		root: srcRoot
+	},
+	dist: {
+		root: distRoot
+	}
+};
+
+gulp.task('clean', function() {
+	return gulp.src(paths.dist.root, {
+			read: false
+		})
+		.pipe(clean());
+});
+
+gulp.task('es2015-to-es5', ['clean'], function() {
+	var entryPath = argv.script || argv.s || srcRoot + '/' +
+			filesUtil.getLastModifiedFileInDir(srcRoot);
+	var outputFileName = argv.output || argv.o || 'scriptOutput.js';
+
+	if (!entryPath) {
+		util.beep();
+		util.log(util.colors.red(
+			'You must provide the path to the script ' +
+			'you want to compile. Use therefor -script or -s'));
+		return;
+	}
+
+	if (!outputFileName) {
+		util.beep();
+		util.log(util.colors.red(
+			'You must provide an output filename. ' +
+			'Use therefor -output or -o'));
+		return;
+	}
+
+	return browserify(entryPath)
+		.transform(babelify, {})
+		.bundle()
+		.on('error', util.log.bind(util, 'Browserify Error'))
+		.pipe(source(outputFileName))
+		.pipe(gulp.dest(distRoot));
+});
+
+gulp.task('default', ['es2015-to-es5']);
