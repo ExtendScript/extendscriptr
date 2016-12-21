584eb81fc9aff03082ef3a393638a0516f52cf15
fabianmoronzirfas <icke@fabianmoronzirfas.me>
2016-12-21 09:33:50 +0100

    minor changes for testing PR

--- test/src/indesign.js
+++ test/src/indesign.js
@@ -1,3 +1,8 @@
+/* globals app, $, SaveOptions */
 import * as lib from './libs/lib';
 
 lib.write(`Hello World ${lib.calc()} ${lib.something}`);
+let doc = app.documents.add();
+doc.pages[0].textFrames.add({geometricBounds: [0,0,100,100],contents:'Hello ID'});
+$.sleep(1000);
+doc.close(SaveOptions.NO);
