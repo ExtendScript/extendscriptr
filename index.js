89469121fd49949cfa81fec20a83f765f9878ee8
fabianmoronzirfas <icke@fabianmoronzirfas.me>
2016-12-21 11:02:18 +0100

    feat(grrrrr): tricking the semantic-release foo

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ var prependify = require('prependify');
 var fs = require('fs');
 
 extendscriptr
-	.version(packageJson.version)
-	.usage('[options]')
-	.option('-s, --script <path>', 'The input file to compile into an executable extendscript')
-	.option('-o, --output <path>', 'The path to the wished compiled output file')
-	.option('-t, --target [targetApp]', 'The Adobe Application the script is intended for. i.e. InDesign [targetApp]')
-	.parse(process.argv);
+  .version(packageJson.version)
+  .usage('[options]')
+  .option('-s, --script <path>', 'The input file to compile into an executable extendscript')
+  .option('-o, --output <path>', 'The path to the wished compiled output file')
+  .option('-t, --target [targetApp]', 'The Adobe Application the script is intended for. i.e. InDesign [targetApp]')
+  .parse(process.argv);
 
 console.log('Running extendscriptr with following options:');
 extendscriptr.options.forEach(function(opt) {
-	if (opt.long === '--version') return;
-	var optionName = opt.long.replace('--', '');
-	console.log(
-		opt.long + ': ' +
-		extendscriptr[optionName] +
-		(opt.optional === 0 ? '' : ' (optional)')
-	);
+    if (opt.long === '--version') return;
+    var optionName = opt.long.replace('--', '');
+    console.log(
+    opt.long + ': ' +
+    extendscriptr[optionName] +
+    (opt.optional === 0 ? '' : ' (optional)')
+  );
 });
 
 var prototypePolyfills = fs.readFileSync('./node_modules/extendscript.prototypes/lib/extendscript.prototypes.js', 'utf8');
@@ -29,26 +29,26 @@ var browserifyPlugins = [ [ prependify, prototypePolyfills ] ];
 
 var adobeTarget = String(extendscriptr.target).toLowerCase();
 if ( adobeTarget &&
-	(adobeTarget.indexOf('indesign') >= 0 ||
-	adobeTarget.indexOf('photoshop') >= 0 ||
-	adobeTarget.indexOf('illustrator') >= 0 ||
-	adobeTarget.indexOf('aftereffects') >= 0)) {
-	browserifyPlugins.push([ prependify, '#target ' + extendscriptr.target + '\n' ]);
+  (adobeTarget.indexOf('indesign') >= 0 ||
+  adobeTarget.indexOf('photoshop') >= 0 ||
+  adobeTarget.indexOf('illustrator') >= 0 ||
+  adobeTarget.indexOf('aftereffects') >= 0)) {
+    browserifyPlugins.push([ prependify, '#target ' + extendscriptr.target + '\n' ]);
 }
 
 var b = browserify({
-	entries: [ extendscriptr.script ],
-	transform: [['babelify', {
-		presets: [
-			'es2015'
-		],
-		plugins: [
-			'babel-plugin-transform-es3-member-expression-literals',
-			'babel-plugin-transform-es3-property-literals',
-			'babel-plugin-transform-es5-property-mutators'
-		]
-	}]],
-	plugin: browserifyPlugins
+    entries: [ extendscriptr.script ],
+    transform: [['babelify', {
+        presets: [
+            'es2015'
+        ],
+        plugins: [
+            'babel-plugin-transform-es3-member-expression-literals',
+            'babel-plugin-transform-es3-property-literals',
+            'babel-plugin-transform-es5-property-mutators'
+        ]
+    }]],
+    plugin: browserifyPlugins
 });
 
 b.bundle().pipe(fs.createWriteStream(extendscriptr.output));
