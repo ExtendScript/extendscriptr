f621549418e5de238fbac244aaf8a4e3b95a3e37
lumenn-home <maxisst@wp.pl>
2019-10-22 22:54:05 +0200

    Switch to extendscript-es5-shim, added some test functions for illustrator

--- test/src/illustrator.js
+++ test/src/illustrator.js
@@ -24,5 +24,74 @@ feel more connected, bringing delight in big and \
 small ways, and empowering them to affect change."';
 
 const fontStyle = text1.textRange.characterAttributes;
-fontStyle.textFont = app.textFonts.getByName("Courier");
+fontStyle.textFont = app.textFonts.getByName("MyriadPro-Bold");
 fontStyle.size = 2.5;
+
+// Tests for Array methods
+
+lib.write('### TESTING ARRAY METHODS ###');
+
+lib.write('Testing map function');
+const testArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1];
+testArray.map(number => lib.write(`map = ${number} out of ${testArray.length - 1} = ${number * 4}`));
+
+const reduceResult = testArray.reduce((acc, curr) => acc + curr, 0);
+lib.write(`Testing reduce function = ${reduceResult}`);
+
+lib.write('Testing forEach function');
+testArray.forEach(value => lib.write(`forEach = ${value}`));
+
+
+lib.write('Testing filter function');
+
+const filteredArray = testArray.filter( (number) => {
+	if(number % 2 === 0) {
+		return true;
+	}
+	return false;
+});
+
+filteredArray.forEach(number => lib.write(`Filter = ${number}`));
+
+lib.write('Testing every function');
+testArray.every(number => {
+	if (number < 3) {
+		lib.write(`Every = ${number}`);
+		return true;
+	} 
+	return false;
+});
+
+lib.write('Testing indexOf function');
+lib.write(testArray.indexOf(4));
+
+lib.write('Testing isArray function');
+const notArray = 10;
+lib.write(Array.isArray(notArray));
+lib.write(Array.isArray(testArray));
+
+lib.write('Testing lastIndexOf function');
+lib.write(testArray.lastIndexOf(1));
+
+lib.write('Testing reduceRight function');
+const reduceRightResult = testArray.reduceRight((acc, curr) => acc + curr, 0);
+lib.write(`Testing reduce function = ${reduceRightResult}`);
+
+
+// Tests for Date methods
+lib.write('### TESTING Date METHODS ###');
+
+const today = new Date();
+lib.write(today.toISOString());
+
+// Tests for Date methods
+lib.write('### TESTING Function METHODS ###');
+lib.write('### TESTING Object METHODS ###');
+
+//TODO implement testing Fuction and Object methods
+
+lib.write('### TESTING String METHODS ###');
+const newString = '          ThisIsSomeWordToTrim          ';
+
+lib.write(`Before trimming: ${newString}`);
+lib.write(`After trimming: ${newString.trim()}`);
